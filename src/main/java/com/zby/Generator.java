e56fad31f2103edefbcd132dfece3dd0d87e768c
zhoubaiyun <zhoubaiyun@haixue.com>
2020-04-01 16:08:20 +0800

    feat：生成ext

--- src/main/java/com/zby/Generator.java
+++ src/main/java/com/zby/Generator.java
@@ -7,7 +7,9 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 
@@ -20,12 +22,15 @@ import org.mybatis.generator.internal.DefaultShellCallback;
  * @author zby
  * @title Generator
  * @date 2019年10月16日
- * @description 【生成完成后需要手动把每个Mapper.xml文件中..替换为.;使用replaceAll；遗留问题待解决】
+ * @description
  */
 public class Generator {
 
-    public static final String sql_target = "sql.target";
-    public static final String sql_package = "sql.package";
+    public static final String MAPPER_TARGET = "mapper.target";
+    public static final String MAPPER_PACKAGE = "mapper.package";
+    public static final String SQL_TARGET = "sql.target";
+    public static final String SQL_PACKAGE = "sql.package";
+    public static final String AUTHOR_NAME = "author.name";
 
     public static void main(String[] args) throws Exception {
         // MBG 执行过程中的警告信息
@@ -47,23 +52,63 @@ public class Generator {
 
         Properties properties = new Properties();
         properties.load(new FileInputStream("src/main/resources/generator.properties"));
-        replace(properties.get(sql_target).toString(), properties.get(sql_package).toString());
+        replaceDot(properties);
+        generateExtXml(properties);
+        generateExtMapper(properties);
     }
 
-    public static void replace(String targetPath, String packagePath) throws Exception {
-        String fileLocation = targetPath + "/" + packagePath.replace(".", "/");
-        File directory = new File(fileLocation);
-        for (File file : directory.listFiles()) {
+    public static void replaceDot(Properties properties) throws Exception {
+        File sqlDirectory = new File(
+                properties.getProperty(SQL_TARGET) + "/" + properties.getProperty(SQL_PACKAGE).replace(".", "/"));
+        for (File file : sqlDirectory.listFiles()) {
             String absolutePath = file.getAbsolutePath();
             StringBuilder sb = new StringBuilder();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
-            bufferedReader.lines().forEach(s -> sb.append(s).append("\r\n"));
+            bufferedReader.lines().forEach(s -> sb.append(s).append("\n"));
             bufferedReader.close();
+            file.delete();
             BufferedWriter bufferedWriter =
                     new BufferedWriter(new OutputStreamWriter(new FileOutputStream(absolutePath)));
             bufferedWriter.write(sb.toString().replaceAll("\\.\\.", "\\."));
             bufferedWriter.close();
         }
-        directory.delete();
     }
+
+    public static void generateExtXml(Properties properties) throws Exception {
+        File sqlDirectory = new File(
+                properties.getProperty(SQL_TARGET) + "/" + properties.getProperty(SQL_PACKAGE).replace(".", "/"));
+        for (File file : sqlDirectory.listFiles()) {
+            String absolutePath = file.getAbsolutePath();
+            BufferedWriter extWriter = new BufferedWriter(
+                    new OutputStreamWriter(new FileOutputStream(absolutePath.replace("Mapper", "MapperExt"))));
+            extWriter.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+                    + "<!DOCTYPE mapper PUBLIC \"-//mybatis.org//DTD Mapper 3.0//EN\" \"http://mybatis.org/dtd/mybatis-3-mapper.dtd\">\n"
+                    + "<mapper namespace=\"" + properties.getProperty(MAPPER_PACKAGE) + "."
+                    + file.getName().substring(0, file.getName().length() - 4) + "Ext" + "\" >\n\n" + "</mapper>");
+            extWriter.close();
+        }
+    }
+
+    public static void generateExtMapper(Properties properties) throws Exception {
+        File mapperDirectory = new File(
+                properties.getProperty(MAPPER_TARGET) + "/" + properties.getProperty(MAPPER_PACKAGE).replace(".", "/"));
+        for (File file : mapperDirectory.listFiles()) {
+            String mapperName = file.getName().substring(0, file.getName().length() - 5);
+            String extName = file.getAbsolutePath().replace("Mapper", "MapperExt");
+            BufferedWriter extWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(extName)));
+            extWriter.write("package " + properties.getProperty(MAPPER_PACKAGE) + ";\n\n");
+            extWriter.write("/**\n");
+            extWriter.write(" * " + mapperName + "拓展\n");
+            extWriter.write(" *\n");
+            extWriter.write(" * @author " + properties.get(AUTHOR_NAME) + "\n");
+            extWriter.write(" * @date " + new SimpleDateFormat("yyyy-MM-dd").format(new Date()) + "\n");
+            extWriter.write(" *\n");
+            extWriter.write(" */\n");
+            extWriter.write("public interface " + mapperName + "Ext extends " + mapperName + "{\n\n}");
+            extWriter.close();
+            System.out.println(extName);
+        }
+
+    }
+
 }
